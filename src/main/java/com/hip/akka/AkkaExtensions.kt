bfa75a342bfc3147a618ef38d7f574cad77a64fd
Marty Pitt <martypitt@gmail.com>
2018-02-21 22:05:08 +0000

    persistence now working

--- src/main/java/com/hip/akka/AkkaExtensions.kt
+++ src/main/java/com/hip/akka/AkkaExtensions.kt
@@ -3,9 +3,20 @@ package com.hip.akka
 import akka.actor.ActorRef
 import akka.pattern.PatternsCS
 import akka.util.Timeout
+import com.hip.utils.log
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.TimeUnit
 
-fun <T> ActorRef.ask(message:Any, timout:Timeout = Timeout.apply(1,TimeUnit.SECONDS)): CompletableFuture<T> {
-   return PatternsCS.ask(this,message,timout).toCompletableFuture() as CompletableFuture<T>
+private object ExceptionHanlder {
+   private val logExceptions = { message: Any, throwable: Exception ->
+      log().error("An exception was thrown during an ask with type ${message::class.java.name}: $throwable")
+   }
+}
+
+fun <T> ActorRef.ask(message: Any, timeout: Timeout = Timeout.apply(1, TimeUnit.SECONDS)): CompletableFuture<T> {
+   return PatternsCS.ask(this, message, timeout).exceptionally { ex ->
+         log().error("An exception was thrown during an Ask: $ex")
+         throw ex
+      }
+      .toCompletableFuture() as CompletableFuture<T>
 }
--- src/main/java/com/hip/akka/AnnotatedActor.kt
+++ src/main/java/com/hip/akka/AnnotatedActor.kt
@@ -28,6 +28,7 @@ internal class AnnotatedReceiveBuilder(val target: AbstractActor) {
          .forEach { method ->
             if (method.parameterCount != 1) throw IllegalArgumentException("Method ${method.name} on ${type.name} must take exactly 1 argument.")
             val paramType = method.parameterTypes[0]
+            method.isAccessible = true
             receive.match(paramType, { param -> method.invoke(target, param) })
          }
       receive.matchAny { message -> log().warn("Received unmatched message of type ${message::class.java.name} which will be ignored") }
@@ -43,7 +44,7 @@ abstract class AnnotatedActor : AbstractActor() {
       return DefaultActorResponse(response)
    }
 
-//   override fun preRestart(reason: Throwable?, message: Option<Any>?) {
+   //   override fun preRestart(reason: Throwable?, message: Option<Any>?) {
 //      super.preRestart(reason, message)
 //   }
 //   override fun postStop() {
@@ -58,33 +59,64 @@ abstract class AnnotatedActor : AbstractActor() {
 abstract class AnnotatedPersistentActor(private val persistenceId: String) : AbstractPersistentActor() {
    override fun persistenceId(): String = persistenceId
 
+   protected var isReplaying = false
    fun <T> reply(response: T): ActorResponse<T> {
       sender.tell(response, self)
       return DefaultActorResponse(response)
    }
 
+   override fun preStart() {
+      this.isReplaying = true
+   }
+   override fun onReplaySuccess() {
+      super.onReplaySuccess()
+      this.isReplaying = false
+   }
+
    protected fun <T, U> persistEvent(event: T, callback: (T) -> U): Mono<U> {
       val future = CompletableFuture<U>()
-      super.persist(event, { persisted ->
-         val result = callback.invoke(persisted)
-         future.complete(result)
-      })
-      return future.toMono()
+      if (isReplaying) {
+         return Mono.just(callback(event))
+      } else {
+         super.persist(event, { persisted ->
+            val result = callback.invoke(persisted)
+            future.complete(result)
+         })
+         return future.toMono()
+      }
+
    }
 
    override fun preRestart(reason: Throwable?, message: Option<Any>?) {
       super.preRestart(reason, message)
    }
+
    override fun postStop() {
       log().error("Actor ${this.javaClass.simpleName} - $this.nam stopping")
       super.postStop()
    }
+
    override fun createReceive(): Receive {
       return AnnotatedReceiveBuilder(this).build()
    }
 
+   override fun onPersistFailure(cause: Throwable?, event: Any?, seqNr: Long) {
+      log().error("Persistence failed")
+      super.onPersistFailure(cause, event, seqNr)
+   }
+
+
+   override fun onPersistRejected(cause: Throwable?, event: Any?, seqNr: Long) {
+      log().error("Persistence rejected")
+      super.onPersistRejected(cause, event, seqNr)
+   }
+
    override fun createReceiveRecover(): Receive {
-      // TODO : We may want to support different annotations for receiveRecover
+      // TODO : We may wish to consider additional behaviours
+      // during replay
       return AnnotatedReceiveBuilder(this).build()
+//      return ReceiveBuilder.create()
+//         .matchAny { message -> log().warn("ReceiveRecover got message: $message") }
+//         .build()
    }
 }
